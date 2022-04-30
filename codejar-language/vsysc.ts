9037f0b1aebea2f3ee7fba988e3dd1de53c667f0
0aoq <73413330+0aoq@users.noreply.github.com>
2022-04-29 22:03:35 -0700

    Create vsysc.ts

---
+++ codejar-language/vsysc.ts
@@ -0,0 +1,20 @@
+export default {
+    "language": {
+        "name": "vsysc",
+        "extentions": [".vsysc", ".vscm"]
+    },
+
+    "keywords": [
+        ["NM", "keyword", "Set document name"],
+        ["DC", "keyword", "Create a variable"],
+        ["WL", "keyword", "Write line"],
+        ["AR", "keyword", "Create an array"],
+        ["AD", "keyword", "Add an item to an array"],
+        ["RM", "keyword", "Remove an item from an array"],
+        ["IM", "function", "Import a file"],
+        ["EX", "function", "Export file"],
+        ["RT", "keyword", "Return a value"]
+    ],
+
+    "snippets": []
+}
