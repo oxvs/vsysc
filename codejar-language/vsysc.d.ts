1811c6fb84e3d2d379eb9e4c0255cf6700205f51
0aoq <73413330+0aoq@users.noreply.github.com>
2022-04-29 22:03:44 -0700

    Create vsysc.d.ts

---
+++ codejar-language/vsysc.d.ts
@@ -0,0 +1,9 @@
+declare const _default: {
+    language: {
+        name: string;
+        extentions: string[];
+    };
+    keywords: string[][];
+    snippets: never[];
+};
+export default _default;
